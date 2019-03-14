95e2fb212fb43ec03ac419eafbdafa1997e9d218
Andrei <gnitkoandrei17@gmail.com>
2019-03-15 00:29:54 +0300

    Initial commit

---
+++ src/main/java/by/teachmeskills/devteam/DevteamApplication.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.devteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevteamApplication.class, args);
+    }
+
+}
