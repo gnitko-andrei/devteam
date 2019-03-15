7fab15aef8208821575f08175013bfea1acb5b87
Andrei <gnitkoandrei17@gmail.com>
2019-03-15 21:18:22 +0300

    dev: Add User and UserRole in database for authorisation. Add simple registration.

---
+++ src/main/java/by/teachmeskills/devteam/controller/RegistrationController.java
@@ -0,0 +1,40 @@
+package by.teachmeskills.devteam.controller;
+
+import by.teachmeskills.devteam.entity.Role;
+import by.teachmeskills.devteam.entity.User;
+import by.teachmeskills.devteam.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Model model) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            model.addAttribute("message", "User already exists!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/by/teachmeskills/devteam/entity/Role.java
@@ -0,0 +1,5 @@
+package by.teachmeskills.devteam.entity;
+
+public enum Role {
+    USER;
+}
