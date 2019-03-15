a93d7e64d3686e563ee354e199e29d9a2fb9c84e
Andrei <gnitkoandrei17@gmail.com>
2019-03-16 01:10:27 +0300

    dev: update security configuration and add customer field for project

--- src/main/java/by/teachmeskills/devteam/controller/MainController.java
+++ src/main/java/by/teachmeskills/devteam/controller/MainController.java
@@ -1,8 +1,10 @@
 package by.teachmeskills.devteam.controller;
 
 import by.teachmeskills.devteam.entity.Project;
+import by.teachmeskills.devteam.entity.User;
 import by.teachmeskills.devteam.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,10 +33,14 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String name, @RequestParam String specification,
-                      @RequestParam String status, Model model) {
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String name,
+            @RequestParam String specification,
+            @RequestParam String status,
+            Model model) {
 
-        Project project = new Project(name, specification, status);
+        Project project = new Project(name, specification, status, user);
         projectRepository.save(project);
 
         Iterable<Project> projects = projectRepository.findAll();
