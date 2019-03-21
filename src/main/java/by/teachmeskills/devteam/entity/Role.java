6f5203c3507a5b083905c0e8381a336e7f3886ef
Andrei <gnitkoandrei17@gmail.com>
2019-03-21 22:35:42 +0300

    dev: add CUSTOMER, MANAGER and DEVELOPER roles

--- src/main/java/by/teachmeskills/devteam/entity/Role.java
+++ src/main/java/by/teachmeskills/devteam/entity/Role.java
@@ -3,7 +3,7 @@ package by.teachmeskills.devteam.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER, ADMIN;
+    USER, ADMIN, CUSTOMER, MANAGER, DEVELOPER;
 
     @Override
     public String getAuthority() {
