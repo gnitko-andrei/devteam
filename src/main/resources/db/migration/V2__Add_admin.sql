032a05ecc767d09862ed3663e755ab0d3c7dbacb
andreihnitsko <970817gA@>
2025-09-02 13:24:54 +0200

    (feat) improve initial load test data

--- src/main/resources/db/migration/V2__Add_admin.sql
+++
@@ -1,6 +0,0 @@
-insert into user (id, username, password, active)
-values (1, 'admin', '$2a$08$5GEDISDmjH1.agxI75qTf.lxMcJWMzq1bdy59hHlv9/3i4a/TOzGG', true); -- password 1
-
-insert into user_role (user_id, roles)
-values (1, 'USER'),
-       (1, 'ADMIN');
