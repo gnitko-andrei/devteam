e3a3236426cda46a1c8d42a288d86cb6e122c898
andreihnitsko <970817gA@>
2025-09-07 14:08:50 +0200

    (test) main controller web mvc tests

---
+++ src/test/java/by/teachmeskills/devteam/controller/MainControllerWebTest.java
@@ -0,0 +1,41 @@
+package by.teachmeskills.devteam.controller;
+
+import by.teachmeskills.devteam.config.SecurityConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(MainController.class)
+@Import(SecurityConfiguration.class)
+class MainControllerWebTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldRenderHome_whenGetRoot() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+
+    @Test
+    void shouldRenderHomeMoreInfo_whenGetInfoProject() throws Exception {
+        mockMvc.perform(get("/info/project"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("homeMoreInfo"));
+    }
+
+    @Test
+    void shouldRenderStackMoreInfo_whenGetInfoStack() throws Exception {
+        mockMvc.perform(get("/info/stack"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("stackMoreInfo"));
+    }
+}
